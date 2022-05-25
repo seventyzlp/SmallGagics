b2861cedf13f3e3ab3cadc2572f17b841ae925d3
giz_soft <15990305624@163.com>
2022-05-25 19:12:39 +0800

    更新约瑟夫环的方法实现凯撒的解密

--- Ceasar_Recode.py
+++ Ceasar_Recode.py
@@ -3,12 +3,23 @@ Code = input('请输入密文：')
 key = int(input('请输入解密口令（默认请输入3）：'))
 STR = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 str = "abcdefghijklmnopqrstuvwxyz"
+asc_A = ord('A')
+asc_a = ord('a')
 ans=''
 for i in Code:
-    if i in STR:
+    if i in STR:  # 大写
         ans += STR[STR.find(i)-key]
-    elif i in str:
+    elif i in str:  # 小写
         ans += str[str.find(i)-key]
     else:
         ans += i
-print(ans)
+print(ans) # 输出字符串取位循环结果
+ans = ''
+for i in Code:
+    if i in STR:  # 大写字符
+        ans += chr((ord(i) - asc_A - key) % 26 + asc_A)  # 这个地方要先减掉基准值，才能在26个字母内循环
+    elif i in str:
+        ans += chr((ord(i) - asc_a - key) % 26 + asc_a)
+    else:
+        ans += i
+print(ans)  # 输出约瑟夫环运算结果
