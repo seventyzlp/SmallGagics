86da41927b5b90dd20882a8d8b035b88e65fd7eb
giz_soft <15990305624@163.com>
2022-06-24 23:11:13 +0800

    汉诺塔图像版本完成，搞一下背景再

--- hanoi_tower.py
+++ hanoi_tower.py
@@ -1,20 +1,70 @@
 #  采用维基百科的图围解法,但是循环方向与维基百科的版本做了一定的区分,采用递归的方式求解
+#  为了实现turtle的效果，打算使用函数的方法来绘制图像
+import turtle
 
 n = int(input("请输入盘片的数量："))
-m = n
+A = [i for i in range(n, 0, -1)]  # A柱，其中数字越大表示的盘片越大
+A.insert(0, "A")
+B = ["B"]
+C = ["C"]
 count = 0
+wide = int(400 / (2 * n))
+color = int(200 / n)
+m = n
+turtle.pensize(wide)
+turtle.setup(600, 400, 0, 0)
+turtle.colormode(255)
+turtle.title('汉诺塔')
+turtle.bgpic('bg.gif')
+# 使用turtle.bgpic设置背景图像
+# 图像初始化
+for i in A:
+    if i != "A":
+        turtle.penup()
+        turtle.goto(50 - 300, wide * 0.5 + wide * (len(A) - 1 - i) - 200)
+        c = color * i
+        turtle.color(c, c, c)
+        turtle.pd()
+        turtle.forward(100)
+        turtle.penup()
 
 
-def hanoi(n, a, b, c):
+def hanoi(n, a, b, c, A, B, C):
     global count
-    global m
     if n == 1:
         count = count + 1
         print(str(count) + ':' + a, '-->', c)  # 最小的那片一直向C移动
+        draw(A, C, A[-1])
+        C.append(A[-1])
+        A.pop()
+        print(str(A))
+        print(str(B))
+        print(str(C))
     else:
-        hanoi(n - 1, a, c, b)
-        hanoi(1, a, b, c)
-        hanoi(n - 1, b, a, c)
+        hanoi(n - 1, a, c, b, A, C, B)  # 把最上面的东西拿到B处
+        hanoi(1, a, b, c, A, B, C)  # 动完上面一堆之后要把最下面那个拿过去
+        hanoi(n - 1, b, a, c, B, A, C)  # 再把之前拿到B处的定西拿回C的上面
 
 
-hanoi(n, 'A', 'B', 'C')
+def draw(a, c, n):
+    # n为盘子序号
+    global wide
+    global color
+    global m
+    turtle.penup()
+    turtle.goto(50 + 200 * (ord(a[0]) - ord('A')) - 300, wide * 0.5 + wide * (m - a[-1]) - 200)  # 这个-100很怪，照理说是200
+    turtle.down()
+    turtle.color("white")  # 先擦除被拿掉的盘子
+    turtle.forward(100)
+    turtle.penup()
+
+    turtle.goto(50 + 200 * (ord(c[0]) - ord('A')) - 300, wide * 0.5 + wide * (m - a[-1]) - 200)  # 在C画新的盘子
+    turtle.down()
+    c = color * n
+    turtle.color(c, c, c)  # 先擦除被拿掉的盘子
+    turtle.forward(100)
+    turtle.penup()
+
+
+hanoi(n, 'A', 'B', 'C', A, B, C)
+turtle.done()
