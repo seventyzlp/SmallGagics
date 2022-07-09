290441f1aaee1cb37546115379592a0516f66dec
张乐平 <15990305624@163.com>
2022-07-09 14:22:33 +0800

    完美版本

--- level_UP.py
+++ level_UP.py
@@ -4,22 +4,23 @@ t = open("test.py", "r+", encoding='utf-8')
 txt = t.read()
 print("原始文件内容是：")
 print(txt.rstrip())
-data = ''
-j = ''
+j = 0
 i = ''
 word = ['']
 # 网络上的方法大多就是新建新文件，替换掉旧文件来实现文字替换，这里是选择修改源文件
 data = txt
-for j in range(len(txt)):
-    if txt[j].islower():
+while j < len(txt):
+    if txt[j] == "\n" or txt[j].isspace():
+        word.append(i)
+        i = ''
+    elif txt[j + 1] == '.' or txt[j + 1] == '(':
+        i = ''
+    elif txt[j].islower():
         i += txt[j]
-    elif word[-1] != 'import':
-        word.append(i)
-        i = ''
-    else:
-        i = ''
-        word.append(i)
+    j += 1
 x = ''
+if 'import' in word:
+    word.remove(word[word.index('import') + 1])
 for x in word:
     if x not in keyword.kwlist:
         y = x.upper()
