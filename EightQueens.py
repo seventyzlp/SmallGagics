c2d86058881999269097672596b27277516538f6
seventy_zlp <15990305624@163.com>
2022-07-11 14:33:30 +0800

    换了csdn的另一个方法

--- EightQueens.py
+++ EightQueens.py
@@ -1,4 +1,7 @@
-def conflict(state, nextColumn):
+import turtle
+
+
+def check(state, nextColumn):
     nextRow = rows = len(state)  # 5
     for row in range(rows):  # 0,1,2,3,4
         # 获取当前行的列
@@ -8,15 +11,12 @@ def conflict(state, nextColumn):
     return False
 
 
-# 采用生成器的方式来产生每一个皇后的位置，并用递归来实现下一个皇后的位置
+# 生成皇后位置
 def queens(num, state=()):
-    # 每一行的列坐标都是从0:7的
-    # 0,1,2,3,4,5,6,7
     for pos in range(num):
         # 默认state为空。长度为0，但是是不冲突的
         # 判断是否冲突，state为空时不冲突
-        if not conflict(state, pos):  # 回溯法的体现
-            # 如果state的长度为7，即到达了倒数第二行，也就是前7行皇后都已经找到了位置，最后一行又没有冲突，返回最后一行的列坐标
+        if not check(state, pos):  # 回溯法
             if len(state) == num - 1:
                 # 最后一行的（pos,）=最后一行的result，然后再递归回去求倒数第二行的result
                 yield (pos,)
@@ -25,15 +25,15 @@ def queens(num, state=()):
                     yield (pos,) + result
 
 
-def prettyprint(solution):
+def prettyprint(solution):  # 生成输出
     def line(pos, length=len(solution)):
         return '.' * (pos) + 'X' + '.' * (length - pos - 1)
-
     for pos in solution:
         print(line(pos))
 
 
-solutions = queens(8)
+n = int(input("请输入皇后的数量:"))
+solutions = queens(n)
 for index, solution in enumerate(solutions):
     print('第%d种解决方案：' % (index + 1), solution)
     prettyprint(solution)
