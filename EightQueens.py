5f0737103bb531f8be5314a364f267d2da7cc877
seventy_zlp <15990305624@163.com>
2022-07-11 12:55:53 +0800

    debug

--- EightQueens.py
+++ EightQueens.py
@@ -1,32 +1,44 @@
 # 使用回溯法，求解N皇后的解法数量，在之后应该会更新turtle版本
 n = int(input("请输入皇后的数量:"))
-pos = [0] * n  # 数组下标是列，数组值是行
-x = n
-j = n
+pos = [0] * (n + 1)  # 数组下标是列，数组值是行,4*4的方格，其中第一行第一列不用
+x = n + 1
+j = n + 1
 
 
 def check(x, y):
     global pos
     flag = True
-    for i in pos:
-        if i == y:
+    for i in range(1,len(pos)):
+        if pos[i] == y:
             flag = False
-        for j in range(i + 1):  # 检测斜着的情况
-            if pos[x - len(pos) * j - 1] != 0 or pos[x - len(pos) * j + 1] != 0:
+        for j in range(1, i + 1):  # 检测斜着的情况,上方
+            a = x - j
+            b = x + j
+            if a <= -1 or b >= len(pos):  # 撞墙了
+                break
+            if pos[a] + 1 == pos[a + 1] or pos[b] + 1 == pos[b - 1]:  # 相同时
                 flag = False
-        for j in range(len(pos) - i):
-            if pos[x + len(pos) * j - 1] != 0 or pos[x + len(pos) * j + 1] != 0:
+        for j in range(1, len(pos) - i):  # 下方
+            a = x - j
+            b = x + j
+            if a <= -1 or b >= len(pos):  # 撞墙了
+                break
+            if pos[a] - 1 == pos[a + 1] or pos[b] - 1 == pos[b - 1]:
                 flag = False
     return flag
 
 
 def place(n):
     global pos
-    global j
-    if j == 0:  # 每一个都安置完毕了
+    if n >= len(pos):  # 每一个都安置完毕了
         print(pos)
     else:
-        for i in range(x):  # 一共要放n个
-            if check(n, i):  # 通过检查可以放下
+        for i in range(1, len(pos)):  # 一共要放n个
+            if check(n + 1, i+1):  # 通过检查可以放下
                 pos[n] = i
-                place(n - 1)
+                n += 1
+                place(n)
+
+
+n = 1
+place(n)
