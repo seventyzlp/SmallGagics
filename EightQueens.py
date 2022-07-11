bad37e51ea8bec75f42a565dcb95542c58500773
seventy_zlp <15990305624@163.com>
2022-07-11 14:12:39 +0800

    debug

--- EightQueens.py
+++ EightQueens.py
@@ -1,70 +1,40 @@
-# 使用回溯法，求解N皇后的解法数量，在之后应该会更新turtle版本
-n = int(input("请输入皇后的数量:"))
-pos = [0] * (n + 1)  # 数组下标是列，数组值是行,4*4的方格，其中第一行第一列不用
+def conflict(state, nextColumn):
+    nextRow = rows = len(state)  # 5
+    for row in range(rows):  # 0,1,2,3,4
+        # 获取当前行的列
+        column = state[row]
+        if abs(column - nextColumn) in [0, nextRow - row]:
+            return True
+    return False
 
 
-def check(x, y):
-    global pos
-    flag = True
-    x -= 1
-    y -= 1
-    for i in range(1, len(pos)):
-        if pos[i] == y:
-            flag = False
-            return flag
-        pos[x] = y
-        for j in range(1, i + 1):  # 检测斜着的情况,上方
-            a = x - j
-            b = x + j
-            # if a <= -1 or b >= len(pos):  # 撞墙了
-            #     break
-            # if pos[a] + 1 == pos[a + 1] or pos[b] + 1 == pos[b - 1]:  # 相同时
-            #     flag = False
-            if a <= 0:
-                if pos[b] + 1 == pos[b - 1]:
-                    flag = False
-                    return flag
-            elif b >= len(pos):
-                if pos[a] + 1 == pos[a + 1]:
-                    flag = False
-                    return flag
+# 采用生成器的方式来产生每一个皇后的位置，并用递归来实现下一个皇后的位置
+def queens(num, state=()):
+    # 每一行的列坐标都是从0:7的
+    # 0,1,2,3,4,5,6,7
+    for pos in range(num):
+        # 默认state为空。长度为0，但是是不冲突的
+        # 判断是否冲突，state为空时不冲突
+        if not conflict(state, pos):  # 回溯法的体现
+            # 如果state的长度为7，即到达了倒数第二行，也就是前7行皇后都已经找到了位置，最后一行又没有冲突，返回最后一行的列坐标
+            if len(state) == num - 1:
+                # 最后一行的（pos,）=最后一行的result，然后再递归回去求倒数第二行的result
+                yield (pos,)
             else:
-                if pos[a] + 1 == pos[a + 1] or pos[b] + 1 == pos[b - 1]:  # 相同时
-                    flag = False
-                    return flag
-        for j in range(1, len(pos) - i - 1):  # 下方
-            a = x - j
-            b = x + j
-            # if a <= -1 or b >= len(pos):  # 撞墙了
-            #     break
-            # if pos[a] - 1 == pos[a + 1] or pos[b] - 1 == pos[b - 1]:
-            #     flag = False
-            if a <= 0:
-                if pos[b] - 1 == pos[b - 1]:
-                    flag = False
-                    return flag
-            elif b >= len(pos):
-                if pos[a] - 1 == pos[a + 1]:
-                    flag = False
-                    return flag
-            else:
-                if pos[a] - 1 == pos[a + 1] or pos[b] - 1 == pos[b - 1]:
-                    flag = False
-                    return flag
-    return flag
+                for result in queens(num, state + (pos,)):
+                    yield (pos,) + result
 
 
-def place(n):
-    global pos
-    if n >= len(pos):  # 每一个都安置完毕了
-        print(pos)
-    else:
-        for i in range(1, len(pos)):  # 一共要放n个
-            if check(n + 1, i + 1):  # 通过检查可以放下
-                pos[n] = i
-                n += 1
-                place(n)
+def prettyprint(solution):
+    def line(pos, length=len(solution)):
+        return '.' * (pos) + 'X' + '.' * (length - pos - 1)
+
+    for pos in solution:
+        print(line(pos))
 
 
-n = 1
-place(n)
+solutions = queens(8)
+for index, solution in enumerate(solutions):
+    print('第%d种解决方案：' % (index + 1), solution)
+    prettyprint(solution)
+    print('*' * 50)
